73fdcc9dc6856d3906fc8d89912680c65fa2779a
charithaallu <51564027+charithaallu@users.noreply.github.com>
2023-11-28 12:09:13 -0500

    Update and rename Jira-automation-script to list_projects.py

---
+++ Jira/list_projects.py
@@ -0,0 +1,26 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://charithaallu.atlassian.net/rest/api/3/project"
+
+API_TOKEN=""
+
+auth = HTTPBasicAuth("", API_TOKEN)
+
+headers = {
+  "Accept": "application/json"
+}
+
+response = requests.request(
+   "GET",
+   url,
+   headers=headers,
+   auth=auth
+)
+
+output = json.loads(response.text)
+
+name = output[0]["name"]
+
+print(name)

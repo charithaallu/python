64931e46ecaa62889784cee9324e603ddaf0e114
charithaallu <51564027+charithaallu@users.noreply.github.com>
2023-11-28 12:14:12 -0500

    Create create_ticket.py

---
+++ Jira/create_ticket.py
@@ -0,0 +1,52 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "provide atlassian.net url"
+
+API_TOKEN = ""
+
+auth = HTTPBasicAuth("", API_TOKEN)
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "automate Jira ticket creation",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "project": {
+      "key": "provide projectkey name"
+    },
+    "issuetype": {
+      "id": "provide issue id"
+    },
+    "summary": "automate Jira ticket creation",
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
